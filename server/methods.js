75381761c006d811f50666437685b30603488261
Chris De Rouck <chris@2minds.be>
2016-02-24 12:39:05 +0100

    basic structure + removal of autopublish/insecure

---
+++ server/methods.js
@@ -0,0 +1,5 @@
+Meteor.methods({
+    clickWire(wireId) {
+        // do some wire clicking
+    }
+});
---
+++ server/publications.js
@@ -0,0 +1,4 @@
+// Share all usernames
+//Meteor.publish(null, function() {
+//    return Meteor.users.find({}, {"fields": { "username": 1}});
+//});
