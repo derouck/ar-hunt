aeec580a759f12acbcb9437b5e4c2c1a6aa334f6
Chris De Rouck <chris@2minds.be>
2016-02-25 15:23:44 +0100

    bomb with data

--- client/templates/bomb/bomb.js
+++ client/templates/bomb/bomb.js
@@ -1,3 +1,10 @@
 Template.currentBomb.onCreated(function(){
    console.log("currentbomb created");
 });
+
+Template.currentBomb.helpers({
+   currentBomb() {
+      console.log(Beacons.findOne());
+      return Beacons.findOne();
+   }
+});
