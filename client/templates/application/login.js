92aba82167a433ed59e30713db652ab47198a124
Chris De Rouck <chris@2minds.be>
2016-02-25 11:43:35 +0100

    some more experimentation with the beacon stuff - still not working

--- client/templates/application/login.js
+++ client/templates/application/login.js
@@ -1,30 +1,9 @@
-var UUID = "d0d3fa86-ca76-45ec-9bd9-6af490c204d4";
-
 Template.login.events({
     'click #logout': function(event) {
-        Meteor.logout(function(err){
+        Meteor.logout(function (err) {
             if (err) {
                 throw new Meteor.Error("Logout failed");
             }
         })
-    },
-    'click #checkBeacons': function(event) {
-        console.log("Test ibeacons with button");
-
-        var reactiveBeaconRegion = new ReactiveBeaconRegion({identifier: "beacons on shelf", uuid: UUID});
-
-        var beaconRegion = reactiveBeaconRegion.getBeaconRegion();
-
-        console.log(beaconRegion);
     }
-});
-
-Meteor.startup(function () {
-    // The correct way
-    console.log("Meteor startup");
-    var reactiveBeaconRegion = new ReactiveBeaconRegion({identifier: "beacons on shelf", uuid: UUID});
-
-    var beaconRegion = reactiveBeaconRegion.getBeaconRegion();
-
-    console.log(beaconRegion);
 });
--- client/templates/layout.js
+++ client/templates/layout.js
@@ -1,12 +1,24 @@
-var UUID = "d0d3fa86-ca76-45ec-9bd9-6af490c204d4";
+var UUID = "B9407F30-F5F8-466E-AFF9-25556B57FE6D";
+var reactiveBeaconRegion;
 
-Template.layout.onCreated(function(){
-    console.log("IBeacons from layout oncreated");
+if(Meteor.isClient){
+    if(Meteor.isCordova) {
+        Template.layout.onCreated(function () {
+            if (Meteor.isCordova) {
+                //var UUID = "d0d3fa86-ca76-45ec-9bd9-6af490c204d4";
+                //UUID = "b9407f30-f5f8-466e-aff9-25556b57fe6d";
+                //UUID = "d0d3fa86-ca76-45ec-9bd9-6af4bbe4d46d";
+                //UUID = "D0D3FA86-CA76-45EC-9BD9-6AF428C8C61F";
+                //UUID = UUID.toUpperCase();
+                reactiveBeaconRegion = new ReactiveBeaconRegion({identifier: "beacons on shelf", uuid: UUID});
+            }
+        });
 
-    var reactiveBeaconRegion = new ReactiveBeaconRegion({identifier: "beacons on shelf", uuid: UUID});
-
-    var beaconRegion = reactiveBeaconRegion.getBeaconRegion();
-
-
-    console.log(beaconRegion);
-});
+        Template.layout.onRendered(function(){
+            this.autorun(function () {
+                var beaconRegion = reactiveBeaconRegion.getBeaconRegion();
+                console.log(beaconRegion);
+            });
+        });
+    }
+};
