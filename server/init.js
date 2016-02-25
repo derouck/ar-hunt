95dce0562b32ef9af47ed60d41d7f657e5082a4e
Jesper Neelen <jesper.neelen@23seconds.be>
2016-02-25 13:58:17 +0000

    Fixing layout

--- server/init.js
+++ server/init.js
@@ -16,28 +16,28 @@ Meteor.startup(function () {
         {
           name:"Bomb 1",
           image:"bomb1.jpg",
-          description:"Try to find me"
+          description:"I’m sitting quite comfortable over here. When I look to the right, I can see the beach and the ocean."
         }
     );
     Beacons.insert(
         {
           name:"Bomb 2",
           image:"bomb2.jpg",
-          description:"Boooommmm!"
+          description:"Don’t look for me inside the hostel, go outside, don’t look too far, then you will find me!"
         }
     );
     Beacons.insert(
         {
           name:"Bomb 3",
           image:"bomb3.jpg",
-          description:"What's your last wish?"
+          description:"Damn, it smells good in here! They even have a stash of beer in this area."
         }
     );
     Beacons.insert(
         {
           name:"Bomb 4",
           image:"bomb4.jpg",
-          description:"I can be activated remotely"
+          description:"Don’t look for me inside the hostel, go outside, take a seat, enjoy the view, take a deep breath, but especially, don’t forget me..."
         }
     );
   }
