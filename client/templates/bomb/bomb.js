66328ccb21aa229d89bbe1de01ac3ebb3c735aba
Alexis d'Oultremont <alexis@qustomer.com>
2016-02-25 17:41:11 +0000

    Multiplayer bomb

--- client/templates/bomb/bomb.js
+++ client/templates/bomb/bomb.js
@@ -1,25 +1,53 @@
+function intersect_safe(a, b)
+{
+  var ai=0, bi=0;
+  var result = new Array();
+
+  while( ai < a.length && bi < b.length )
+  {
+     if      (a[ai] < b[bi] ){ ai++; }
+     else if (a[ai] > b[bi] ){ bi++; }
+     else /* they're equal */
+     {
+       result.push(a[ai]);
+       ai++;
+       bi++;
+     }
+  }
+
+  return result;
+}
+
 Template.currentBomb.onCreated(function(){
 
     bomb = Beacons.findOne();
     instance = Template.instance();
     instance.beaconsFound = new ReactiveVar( false );
-
+    instance.Bomb = bomb;
     if (Meteor.isCordova) {
 
       region = new ReactiveBeaconRegion({identifier: bomb.name, uuid: bomb.uuid});
 
       self = this;
-      Tracker.autorun(function () {
+      instance.autorun(function () {
 
         if (region.getBeaconRegion().beacons.length) {
             let beacons = region.getBeaconRegion().beacons;
-            console.log(beacons);
+            //console.log(beacons);
             //Template.instance().beaconsFound.set();
             _.each(beacons, function(element){
-                console.log(element.uuid, element.minor, element.major);
+                //console.log(element.uuid, element.minor, element.major);
                 if(element.minor == bomb.minor && element.major == bomb.major){
                   instance.beaconsFound.set(element.proximity);
-                  console.log("beacon in region "+bomb.minor+" "+bomb.major);
+                  //console.log("beacon in region "+bomb.minor+" "+bomb.major);
+
+
+                  if(element.proximity == 'ProximityImmediate' || element.proximity == 'ProximityNear'){
+                    Meteor.call('updateNearestBeacon', bomb._id);
+                  }
+                  else {
+                    Meteor.call('updateNearestBeacon', null);
+                  }
                 }
             });
 
@@ -31,7 +59,7 @@ Template.currentBomb.onCreated(function(){
 
 Template.currentBomb.helpers({
    currentBomb() {
-      console.log(Beacons.findOne());
+      console.log();
       return Beacons.findOne();
    },
    proximity: function(){
@@ -53,7 +81,26 @@ Template.currentBomb.helpers({
   proximity0: function(){
    return (Template.instance().beaconsFound.get() =='ProximityUnknown');
    //return Session.get('subBeaconList');
-  }
+ },
+ nearByUsers: function(){
+   //console.log(this.Bomb._id);
+   let game = Games.findOne();
+   let bomb = Beacons.findOne();
+   let currentUsers = Meteor.users.find({nearestBeacon:bomb._id}).fetch();
+   let currentUsersIds = _.map(currentUsers, function(value, key){ return value._id});
+   let currentUsersOfGame = _.intersection(currentUsersIds, game.players)
+   console.log(currentUsersIds, game.players, currentUsersOfGame);
+   return currentUsersOfGame;
+ },
+ canCut: function(){
+   let game = Games.findOne();
+   let bomb = Beacons.findOne();
+   let currentUsers = Meteor.users.find({nearestBeacon:bomb._id}).fetch();
+   let currentUsersIds = _.map(currentUsers, function(value, key){ return value._id});
+   let currentUsersOfGame = _.intersection(currentUsersIds, game.players)
+
+   return (currentUsersOfGame.length == game.players.length);
+ }
 });
 
 Template.currentBomb.events({
@@ -63,6 +110,10 @@ Template.currentBomb.events({
     Meteor.call('cutWire', bomb._id, function(err, result){
 
       console.log(result);
-    })
+    });
   }
+});
+
+Template.currentBomb.onDestroyed(function () {
+  Meteor.call('updateNearestBeacon', null);
 });
